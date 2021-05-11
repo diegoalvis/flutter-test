0b4f03b2d840d747b2ef2d4d7a054213558f6a65
William Alvis <alvis.wap@gmail.com>
2021-05-11 16:55:58 -0500

    permitiendo establecer el color del puntero en el custom TextField

--- lib/widget/text_field.dart
+++ lib/widget/text_field.dart
@@ -6,25 +6,28 @@ class TextFieldCustom extends StatelessWidget {
   const TextFieldCustom({
     Key? key,
     required TextEditingController controller,
+    this.cursorColor:IdtColors.blackShadow,
     required this.decoration, required this.style, this.keyboardType: TextInputType.text, this.obscureText :false,
   })
-      : _controllerEmail = controller,
+      : _controller = controller,
         super(key: key);
 
-  final TextEditingController _controllerEmail;
+  final TextEditingController _controller;
   final InputDecoration decoration;
   final TextStyle style;
   final TextInputType  keyboardType;
   final bool obscureText;
+  final Color cursorColor;
+
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       obscureText: obscureText,
-      cursorColor: IdtColors.blackShadow,
+      cursorColor: cursorColor,
       keyboardType: keyboardType,
       style: style,
-      controller: _controllerEmail,
+      controller: _controller,
       decoration: decoration,
     );
   }
