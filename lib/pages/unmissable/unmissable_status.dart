aedc41a59aeb4f86f579fec37ac637de8a520e15
William Alvis <alvis.wap@gmail.com>
2021-05-23 10:36:52 -0500

    Manejando request de la data atravez del ViewModel y tambien marcando la opcion selecionada

--- lib/pages/unmissable/unmissable_status.dart
+++ lib/pages/unmissable/unmissable_status.dart
@@ -1,21 +1,31 @@
 import 'package:bogota_app/data/model/data_model.dart';
 import 'package:bogota_app/view_model.dart';
 
-class UnmissableStatus extends ViewStatus{
-
+class UnmissableStatus extends ViewStatus {
+  final int currentOption;
   final bool isLoading;
   final bool openMenu;
   late List<DataModel> itemsUnmissablePlaces;
   late List<DataModel> itemsbestRatedPlaces;
 
-  UnmissableStatus({required this.isLoading, required this.openMenu,required this.itemsUnmissablePlaces, required this.itemsbestRatedPlaces});
+  UnmissableStatus(
+      {required this.currentOption,
+      required this.isLoading,
+      required this.openMenu,
+      required this.itemsUnmissablePlaces,
+      required this.itemsbestRatedPlaces});
 
-  UnmissableStatus copyWith({bool? isLoading, bool? openMenu,List<DataModel>? itemsUnmissablePlaces, List<DataModel>?itemsbestRatedPlaces}) {
+  UnmissableStatus copyWith(
+      {int? currentOption,
+      bool? isLoading,
+      bool? openMenu,
+      List<DataModel>? itemsUnmissablePlaces,
+      List<DataModel>? itemsbestRatedPlaces}) {
     return UnmissableStatus(
-      isLoading: isLoading ?? this.isLoading,
-      openMenu: openMenu ?? this.openMenu,
-      itemsUnmissablePlaces: itemsUnmissablePlaces ?? this.itemsUnmissablePlaces,
-      itemsbestRatedPlaces: itemsbestRatedPlaces??this.itemsbestRatedPlaces
-    );
+        currentOption: currentOption ?? this.currentOption,
+        isLoading: isLoading ?? this.isLoading,
+        openMenu: openMenu ?? this.openMenu,
+        itemsUnmissablePlaces: itemsUnmissablePlaces ?? this.itemsUnmissablePlaces,
+        itemsbestRatedPlaces: itemsbestRatedPlaces ?? this.itemsbestRatedPlaces);
   }
 }
