542838b048e4cffefd481da63af4624bec4035a2
William Alvis <alvis.wap@gmail.com>
2021-06-10 21:24:20 -0500

    se hace cambio del titulo del filtro al selecionr la localidad

--- lib/pages/events/events_view_model.dart
+++ lib/pages/events/events_view_model.dart
@@ -86,9 +86,11 @@ class EventsViewModel extends EffectsViewModel<EventsStatus, EventsEffect> {
     final Map query = {'zone' : item.id};
     // print('1. $query');
 
+
     final response = await _interactor.getPlaceEventForLocation(query, section);
     if (response is IdtSuccess<List<DataModel>?>) {
       status = status.copyWith(places: response.body); // Status reasignacion
+      status = status.copyWith(nameFilter: item.title!);
 
     } else {
       final erroRes = response as IdtFailure<FilterError>;
