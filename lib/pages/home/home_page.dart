e10a7c674b4278f46e72567011c58c569c62b929
William Alvis <alvis.wap@gmail.com>
2021-06-30 20:07:51 -0500

    Creacion de widget NFMarquee, Para hacer un desplazamiento cuando el Texto se desborde de  su contenedor

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:bogota_app/commons/idt_assets.dart';
 import 'package:bogota_app/data/repository/interactor.dart';
 import 'package:bogota_app/commons/idt_colors.dart';
@@ -11,6 +12,7 @@ import 'package:bogota_app/pages/home/home_effect.dart';
 import 'package:bogota_app/pages/home/home_view_model.dart';
 import 'package:bogota_app/extensions/idt_dialog.dart';
 import 'package:bogota_app/utils/local_data/box.dart';
+import 'package:bogota_app/widget/NFMarquee.dart';
 import 'package:bogota_app/widget/appbar.dart';
 import 'package:bogota_app/widget/bottom_appbar.dart';
 import 'package:bogota_app/widget/fab.dart';
@@ -20,6 +22,7 @@ import 'package:bogota_app/widget/idt_progress_indicator.dart';
 import 'package:bogota_app/widget/menu.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:marquee/marquee.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
@@ -166,18 +169,22 @@ class _HomeWidgetState extends State<HomeWidget> {
                               child: Row(
                                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                 children: [
-                                  Text(
-                                    optionsHomeList[index].toUpperCase(),
-                                    style: TextStyle(
-                                        color: IdtColors.white,
-                                        fontSize: 26,
-                                        fontWeight: FontWeight.bold,
-                                        shadows: [
-                                          Shadow(
-                                              color: Colors.black.withOpacity(0.9),
-                                              offset: Offset(3, 2),
-                                              blurRadius: 3),
-                                        ]),
+                                  Expanded(
+                                    child: NFMarquee(
+                                      text: optionsHomeList[index].toUpperCase(),
+                                      fontSize: 26,
+                                      fontWeight: FontWeight.bold,
+                                      style: TextStyle(
+                                          color: IdtColors.white,
+                                          fontSize: 26,
+                                          fontWeight: FontWeight.bold,
+                                          shadows: [
+                                            Shadow(
+                                                color: Colors.black.withOpacity(0.9),
+                                                offset: Offset(3, 2),
+                                                blurRadius: 3),
+                                          ]),
+                                    ),
                                   ),
                                   Icon(
                                     Icons.navigate_next,
@@ -232,41 +239,3 @@ class _HomeWidgetState extends State<HomeWidget> {
 
   List<String> optionsHomeList = ['Descubre Bogotá', 'Eventos', '¿Dónde comer?', '¿Dónde Dormir?'];
 }
-
-// Container(
-//   height: 140,
-//   decoration: BoxDecoration(
-//     color: Colors.red.withOpacity(0.4),
-//   ),
-//   child: Stack(
-//     children: [
-//       Center(
-//         child: Padding(
-//           padding: EdgeInsets.symmetric(horizontal: 12),
-//           child: Row(
-//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//             children: [
-//               Text(
-//                 optionsHome[index].toUpperCase(),
-//                 style: TextStyle(
-//                     color: IdtColors.white,
-//                     fontSize: 26,
-//                     fontWeight: FontWeight.bold),shadows: [
-//                                       Shadow(
-//                                           color: Colors.black.withOpacity(0.7),
-//                                           offset: Offset(3, 2),
-//                                           blurRadius: 3),
-//                                     ]
-//               ),
-//               Icon(
-//                 IdtIcons.compass,
-//                 color: Colors.white,
-//                 size: 26,
-//               ),
-//             ],
-//           ),
-//         ),
-//       )
-//     ],
-//   ),
-// ),
